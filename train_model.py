35ce144addb39d90a74079e0815d27347eb41f8e
LynnHunt123 <linruoyu1202@gmail.com>
2022-01-19 14:24:01 -0500

    updated py scripts and notebook, added newest logs

---
+++ train_model.py
@@ -0,0 +1,248 @@
+#TODO: Import your dependencies.
+#For instance, below are some dependencies you might need if you are using Pytorch
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.models as models
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+
+import argparse
+import time
+import os
+import logging
+import sys
+from tqdm import tqdm
+# from smdebug import modes
+# from smdebug.profiler.utils import str2bool
+# from smdebug.pytorch import get_hook
+
+from PIL import ImageFile
+
+import json
+
+from PIL import Image
+import io
+import requests
+
+JSON_CONTENT_TYPE = 'application/json'
+JPEG_CONTENT_TYPE = 'image/jpeg'
+
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+logger=logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler(sys.stdout))
+
+#TODO: Import dependencies for Debugging andd Profiling
+
+def test(model, test_loader, criterion):
+    model.eval()
+    running_loss=0
+    running_corrects=0
+    
+    for inputs, labels in test_loader:
+        outputs=model(inputs)
+        loss=criterion(outputs, labels)
+        _, preds = torch.max(outputs, 1)
+        running_loss += loss.item() * inputs.size(0)
+        running_corrects += torch.sum(preds == labels.data)
+
+    total_loss = running_loss // len(test_loader)
+    total_acc = running_corrects.double() // len(test_loader)
+    
+    logger.info(f"Testing Loss: {total_loss}")
+    logger.info(f"Testing Accuracy: {total_acc}")
+    pass
+
+def train(model, train_loader,valid_loader, criterion, optimizer):
+#     hook = get_hook(create_if_not_exists=True)
+    epochs=50
+    best_loss=1e6
+    image_dataset={'train':train_loader, 'valid':valid_loader}
+    loss_counter=0
+    
+    for epoch in range(epochs):
+        logger.info(f"Epoch: {epoch}")
+        for phase in ['train', 'valid']:
+            if phase=='train':
+                model.train()
+            else:
+                model.eval()
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in image_dataset[phase]:
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                if phase=='train':
+                    optimizer.zero_grad()
+                    loss.backward()
+                    optimizer.step()
+
+                _, preds = torch.max(outputs, 1)
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss // len(image_dataset[phase])
+            epoch_acc = running_corrects // len(image_dataset[phase])
+            
+            if phase=='valid':
+                if epoch_loss<best_loss:
+                    best_loss=epoch_loss
+                else:
+                    loss_counter+=1
+
+
+            logger.info('{} loss: {:.4f}, acc: {:.4f}, best loss: {:.4f}'.format(phase,
+                                                                                 epoch_loss,
+                                                                                 epoch_acc,
+                                                                                 best_loss))
+        if loss_counter==1:
+            break
+        if epoch==0:
+            break
+    return model
+    
+def net():
+    model = models.resnet50(pretrained=True)
+
+    for param in model.parameters():
+        param.requires_grad = False   
+
+    model.fc = nn.Sequential(
+                   nn.Linear(2048, 128),
+                   nn.ReLU(inplace=True),
+                   nn.Linear(128, 133))
+    return model
+
+
+def create_data_loaders(data, batch_size):
+    train_data_path = os.path.join(data, 'train')
+    test_data_path = os.path.join(data, 'test')
+    validation_data_path=os.path.join(data, 'valid')
+
+    train_transform = transforms.Compose([
+        transforms.RandomResizedCrop((224, 224)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        ])
+
+    test_transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        ])
+
+    train_data = torchvision.datasets.ImageFolder(root=train_data_path, transform=train_transform)
+    train_data_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+    test_data = torchvision.datasets.ImageFolder(root=test_data_path, transform=test_transform)
+    test_data_loader  = torch.utils.data.DataLoader(test_data, batch_size=batch_size, shuffle=True)
+
+    validation_data = torchvision.datasets.ImageFolder(root=validation_data_path, transform=test_transform)
+    validation_data_loader  = torch.utils.data.DataLoader(validation_data, batch_size=batch_size, shuffle=True) 
+    
+    return train_data_loader, test_data_loader, validation_data_loader
+
+###############################################################################################
+# Mute everything after experimentation
+
+def model_fn(model_dir):
+    print("In model_fn. Model directory is -", model_dir)
+    device = "cpu"
+    model = net().to(device)
+#     model = Net()
+    
+    with open(os.path.join(model_dir, "model.pth"), "rb") as f:
+        print("Loading the dog-classifier model")
+        print(f"Is f defined? type(f) = {type(f)}, f = {f}.")
+        checkpoint = torch.load(f , map_location =device)
+#         checkpoint = torch.load(f)
+        print("checkpoint defined.")
+        model.load_state_dict(checkpoint)
+        print('MODEL-LOADED')
+        logger.info('model loaded successfully')
+    model.eval()
+    return model
+
+def input_fn(request_body, content_type=JPEG_CONTENT_TYPE):
+    logger.info('Deserializing the input data.')
+    # process an image uploaded to the endpoint
+    #if content_type == JPEG_CONTENT_TYPE: return io.BytesIO(request_body)
+    logger.debug(f'Request body CONTENT-TYPE is: {content_type}')
+    logger.debug(f'Request body TYPE is: {type(request_body)}')
+    if content_type == JPEG_CONTENT_TYPE: return Image.open(io.BytesIO(request_body))
+    logger.debug('SO loded JPEG content')
+    # process a URL submitted to the endpoint
+    
+    if content_type == JSON_CONTENT_TYPE:
+        #img_request = requests.get(url)
+        logger.debug(f'Request body is: {request_body}')
+        request = json.loads(request_body)
+        logger.debug(f'Loaded JSON object: {request}')
+        url = request['url']
+        img_content = requests.get(url).content
+        return Image.open(io.BytesIO(img_content))
+    
+    raise Exception('Requested unsupported ContentType in content_type: {}'.format(content_type))
+
+# inference
+def predict_fn(input_object, model):
+    logger.info('In predict fn')
+    test_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    ])
+    logger.info("transforming input")
+    input_object=test_transform(input_object)
+    
+    with torch.no_grad():
+        logger.info("Calling model")
+        prediction = model(input_object.unsqueeze(0))
+    return prediction
+
+
+###############################################################################################
+
+
+def main(args):
+
+    logger.info(f'Trial - HPs are LR: {args.learning_rate}, Batch Size: {args.batch_size}')
+    logger.info(f'Data Paths: {args.data}')
+    train_loader, test_loader, valid_loader = create_data_loaders(args.data, args.batch_size)
+    model=net()
+        
+    criterion = nn.CrossEntropyLoss(ignore_index=133)
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.learning_rate)
+    
+
+    logger.info("Training...")
+    model=train(model, train_loader, valid_loader, criterion, optimizer)
+    
+
+    logger.info("Testing...")
+    test(model, test_loader, criterion)
+
+
+    logger.info("Saving...")
+#     torch.save(model.cpu().state_dict(), os.path.join(args.model_dir, "model.pth"))
+    path = os.path.join(args.model_dir, "model.pth")
+    torch.save(model.cpu().state_dict(), path)
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    '''
+    TODO: Specify any training args that you might need
+    '''
+    parser.add_argument('--learning_rate', type=float)
+    parser.add_argument('--batch_size', type=int)
+    parser.add_argument('--data', type=str, default=os.environ['SM_CHANNEL_TRAINING'])
+    parser.add_argument('--model_dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    parser.add_argument('--output_dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    
+    args=parser.parse_args()
+    
+    main(args)
